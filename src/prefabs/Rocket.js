b4685dfc0175b05c9bc050ccac531dee9d8acbb5
maya.lunaa <malubrow@ucsc.edu>
2022-04-06 23:20:04 -0700

    Added movement

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,12 +1,12 @@
 // Rocket prefab
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
-      super(scene, x, y, texture, frame);
+        super(scene, x, y, texture, frame);
   
-      // add object to existing scene
-      scene.add.existing(this);
-      this.isFiring = false;
-      this.moveSpeed = 2;
+        // add object to existing scene
+        scene.add.existing(this);
+        this.isFiring = false;
+        this.moveSpeed = 2;
     }
 
     update() {
