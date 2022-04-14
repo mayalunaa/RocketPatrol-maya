886521f10f2486a2346ac1d818fcdcd6a34a003b
maya.lunaa <malubrow@ucsc.edu>
2022-04-14 01:18:46 -0700

    Added main.js comment

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
+/*
+Maya Brown
+Rocket Patrol Improvment
+4/14/2022
+Hours Spent to Complete: ~8-9 hours
+
+Modifications made: (total of 100 points)
+-------------------------------------------------------------------
+1.) Simultaneous two-player mode (30 points)
+2.) Smaller, faster spaceship worth more points (20 points)
+3.) New artwork for rockets, spaceships and explosion (20 points)
+4.) Successful hits add 1 second to the clock (20 points)
+5.) Timer display (10 points)
+*/
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
